4bb6065eacad52382c277efe80a0a1b818f343b5
Arshad <77781954+mdArshad10@users.noreply.github.com>
2023-05-09 11:14:13 -0400

    Update app.js
    remove the " " in origin

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(morgan('common'))
 app.use(helmet())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: http://localhost:5173,
     credentials: true,
     methods: ["GET","POST","PUT","DLETE"],
 }))
